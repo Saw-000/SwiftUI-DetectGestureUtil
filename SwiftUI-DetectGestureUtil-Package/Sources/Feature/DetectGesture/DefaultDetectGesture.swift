f70044490f93b2a6f60c529ef62e20893cfefd21
Ie So <zousaaaaaan@gmail.com>
2025-11-02 14:59:10 +0900

    gitignoreキャッシュを削除

--- SwiftUI-DetectGestureUtil-Package/Sources/Feature/DetectGesture/DefaultDetectGesture.swift
+++
@@ -1,9 +0,0 @@
-/// Default Gestures
-
-import Foundation
-public enum DefaultDetectGesture {
-    case tap
-//    case sequentialTap(count: Int)
-    case longTap(minimumMilliSeconds: TimeInterval)
-//    case drag(minimumDistance: CGFloat)
-}
