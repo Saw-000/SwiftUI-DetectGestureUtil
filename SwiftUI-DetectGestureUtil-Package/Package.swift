35bd04793dad71d334ab13602b38c6c98ee0b347
Ie So <zousaaaaaan@gmail.com>
2025-11-03 00:50:14 +0900

    要らない依存を消す

--- SwiftUI-DetectGestureUtil-Package/Package.swift
+++ SwiftUI-DetectGestureUtil-Package/Package.swift
@@ -58,36 +58,3 @@ enum MyModule {
         return .byName(name: name, condition: nil)
     }
 }
-
-/** 外部ライブラリ */
-struct ThirdParty {
-    /** 外部ライブラリパッケージ */
-    enum Package {
-        case swiftComposableArchitecture
-        case swiftAlgorithms
-
-        var dependency: PackageDescription.Package.Dependency {
-            return switch self {
-            case .swiftComposableArchitecture:
-                .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.23.1")
-            case .swiftAlgorithms:
-                .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
-            }
-        }
-    }
-
-    /** 外部ライブラリプロダクト */
-    enum Product {
-        case swiftComposableArchitecture
-        case swiftAlgorithms
-
-        var targetDependency: Target.Dependency {
-            return switch self {
-            case .swiftComposableArchitecture:
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-            case .swiftAlgorithms:
-                .product(name: "Algorithms", package: "swift-algorithms")
-            }
-        }
-    }
-}
