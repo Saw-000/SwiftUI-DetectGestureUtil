818aba6d7c65c5fe9dece841fa98c2aaf676104b
Ie So <zousaaaaaan@gmail.com>
2026-01-02 02:32:09 +0900

    SwiftFormatを導入する

---
+++ SwiftFormatPackage/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "SwiftFormatPackage",
+	products: [
+		.library(name: "SwiftFormatPackage", targets: ["SwiftFormatPackage"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/nicklockwood/SwiftFormat", .upToNextMajor(from: "0.58.7"))
+	],
+	targets: [
+		.target(
+			name: "SwiftFormatPackage",
+			dependencies: []
+		)
+	]
+)
